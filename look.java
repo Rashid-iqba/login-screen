bf433b76e457aec9f107c630aa4154d13c077d4e
RASHID IQBAL <45360835+Rashid-iqba@users.noreply.github.com>
2019-03-04 02:11:26 +0530

    Create look.java

---
+++ look.java
@@ -0,0 +1,26 @@
+class L
+{
+	public int Roll,int Name
+
+	public void Set(int x,int y)
+	{
+		x=Roll; y=Name;
+	}
+	public void Shows()
+	{
+		System.out.println("my name is ="+ Name);
+		System.out.println("roll number is="+ Roll);
+	}
+}
+
+class roll
+{
+     public static void main(String[] args) 
+     {
+
+     	L l=new L();
+     	i.Set(57, 89);
+     	i.Shows();
+		
+	}
+}
